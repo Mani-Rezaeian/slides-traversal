0281d5aa5e21625de5e2e9c67864433e85617a6e
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-04-03 14:10:52 -0400

    next slide functionality in place.

--- js/index.js
+++ js/index.js
@@ -1,33 +1,71 @@
-const $show = document.querySelector('.show')
+let $show = document.querySelector('.show')
 
 
 // A function that goes to the start of the show
-const setFirstSlide = () => {
+let setFirstSlide = () => {
+
+	// Find first element
+	let $first = document.querySelector('.slide:first-child')
+
+	// Adds a class to it
+	$first.classList.add('current')
 
 }
 
+
 // A function that goes to the end of the show
-const setLastSlide = () => {
+let setLastSlide = () => {
 
 }
 
 // Remove ".current" from all ".slide"
-const unsetSlides = () => {
+let unsetSlides = () => {
+	// 1. Locate the current slide
+	let $curr = document.querySelector('.current')
 
+	// 2. Remove the current class
+	$curr.classList.remove('current')
+
+	return $curr
 }
 
 // Previous slide
-const prevSlide = () => {
+let prevSlide = () => {
 
 }
 
 // Next slide
-const nextSlide = () => {
+let nextSlide = () => {
+
+	let $curr = unsetSlides()
+
+	// 3. Find the next sibling slide
+	let $next = $curr.nextElementSibling
+
+	if ($next != null) {
+		// 4. If it exists, add the current class
+		$next.classList.add('current')
+	} else {
+		// 5. If it doesn't exist, go back to the beginning
+		setFirstSlide()
+	}
 
 }
 
 // When the interface has fully loaded...
-const windowLoaded = () => {
+let windowLoaded = () => {
+	// Check if there's a hash set, if so start at that slide
+	// window.location.hash
+
+	// else, use the first slide
+	// Setup the first slide
+	setFirstSlide()
+
+
+	// Add all the event listeners
+	document.querySelector('#next').addEventListener('click', nextSlide)
 
 }
 
+
+window.addEventListener('load', windowLoaded)
